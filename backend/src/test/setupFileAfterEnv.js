606571befe0848f9a2505c1d3697ae58a634576e
Carleigh <158855829+carlphelon@users.noreply.github.com>
2025-09-16 22:53:08 +0000

    chore: integrate jest

---
+++ backend/src/test/setupFileAfterEnv.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose'
+
+import { beforeAll, afterAll } from '@jest/globals'
+
+import { initDatabase } from '../db/init.js'
+
+beforeAll(async () => {
+  await initDatabase()
+})
+
+afterAll(async () => {
+  await mongoose.disconnect()
+})
