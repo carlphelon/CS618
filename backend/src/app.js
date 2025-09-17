854cb9f9d8c680596ec0d2882ea7f39bf7c99cf6
Carleigh <158855829+carlphelon@users.noreply.github.com>
2025-09-17 21:08:05 +0000

    chore: enabling CORS in API

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,16 @@
 import express from 'express'
+import { postsRoutes } from './routes/posts.js'
+import bodyParser from 'body-parser'
+import cors from 'cors'
 
 const app = express()
+app.use(bodyParser.json())
+app.use(cors())
+
+postsRoutes(app)
+
 app.get('/', (req, res) => {
-  res.send('Hello from Express!')
+  res.send('Hello from Express2!')
 })
 
 export { app }
