3d3820d3863390c9a49865961c3d0ac2827a9b72
Carleigh <158855829+carlphelon@users.noreply.github.com>
2025-09-17 22:55:18 +0000

    feat: implement filters and sorting

--- backend/src/example.js
+++ backend/src/example.js
@@ -2,23 +2,19 @@ import { initDatabase } from './db/init.js'
 
 import { Post } from './db/models/post.js'
 
+import dotenv from 'dotenv'
+dotenv.config()
+
 await initDatabase()
 
 const post = new Post({
-  title: 'Hello Mongoose!',
-
-  author: 'Huseyin Ergin',
-
-  contents: 'This post is stored in a MongoDB database using Mongoose.',
-
-  tags: ['mongoose', 'mongodb'],
+  title: 'Hello from the outside!',
+  author: 'Some Person',
+  contents: 'This is a new content.',
+  tags: ['frontend'],
 })
-const createdPost = await post.save()
 
-await Post.findByIdAndUpdate(createdPost._id, {
-  $set: { title: 'Hello again, Mongoose!' },
-})
+await post.save()
 
 const posts = await Post.find()
-
 console.log(posts)
